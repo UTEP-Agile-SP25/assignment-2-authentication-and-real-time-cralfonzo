6f5579623969ecbc31654eda0b388386f5666832
cralfonzo <karlols333@gmail.com>
2025-03-07 15:36:04 -0700

    Finished

--- src/cityManager.js
+++ src/cityManager.js
@@ -3,65 +3,65 @@ import { db } from "./config";
 import { doc, setDoc, collection, deleteDoc, onSnapshot } from "firebase/firestore";
 
 const saveSong = async function () {
-    const songName = document.getElementById("songname").value.trim();
-    const artist = document.getElementById("artist").value.trim();
-    const releaseYear = document.getElementById("releaseYear").value.trim();
+    const songName = document.getElementById("songname").value.trim()
+    const artist = document.getElementById("artist").value.trim()
+    const releaseYear = document.getElementById("releaseYear").value.trim()
 
     try {
-        const songRef = doc(db, "songs", songName.toLowerCase()+ "-" + artist.toLowerCase());
+        const songRef = doc(db, "songs", songName.toLowerCase()+ "-" + artist.toLowerCase())
 
         await setDoc(songRef, {
             name: songName,
             artist: artist,
             releaseYear: releaseYear,
             time: new Date()
-        });
-        console.log("Song successfully added");
-        document.getElementById("songname").value = "";
-        document.getElementById("artist").value = "";
-        document.getElementById("releaseYear").value = "";
+        })
+        console.log("Song successfully added")
+        document.getElementById("songname").value = ""
+        document.getElementById("artist").value = ""
+        document.getElementById("releaseYear").value = ""
     } catch (error) {
-        console.error("Error saving song: ", error);
+        console.error("Error saving song: ", error)
     }
-};
+}
 
 const deleteSong = async function (collection, docID) {
     try {
-        await deleteDoc(doc(db, collection, docID));
-        console.log(`Document with ID ${docID} deleted successfully`);
+        await deleteDoc(doc(db, collection, docID))
+        console.log(`Document with ID ${docID} deleted successfully`)
     } catch (error) {
-        console.error("Error deleting song", error);
+        console.error("Error deleting song", error)
     }
-};
+}
 
-const songCollection = collection(db, "songs");
+const songCollection = collection(db, "songs")
 onSnapshot(songCollection, (snapshot) => {
-    const tableBody = document.getElementById("table-body");
-    tableBody.innerHTML = "";
+    const tableBody = document.getElementById("table-body")
+    tableBody.innerHTML = ""
 
     snapshot.forEach((doc) => {
-        const data = doc.data();
-        const row = document.createElement("tr");
+        const data = doc.data()
+        const row = document.createElement("tr")
 
         row.innerHTML = `
         <td> ${doc.id}</td>
         <td> ${data.name}</td>
         <td> ${data.artist}</td>
         <td> ${data.releaseYear}</td>
-        `;
-        tableBody.appendChild(row);
-    });
-});
+        `
+        tableBody.appendChild(row)
+    })
+})
 
-const addSongForm = document.querySelector("#addSong");
+const addSongForm = document.querySelector("#addSong")
 addSongForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    saveSong();
-});
+    event.preventDefault()
+    saveSong()
+})
 
-const deleteSongForm = document.querySelector("#deleteSong");
+const deleteSongForm = document.querySelector("#deleteSong")
 deleteSongForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const song = document.getElementById("songID").value;
-    deleteSong("songs", song);
-});
+    event.preventDefault()
+    const song = document.getElementById("songID").value
+    deleteSong("songs", song)
+})
